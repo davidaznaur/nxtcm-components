ee3c97f80b2d9b29ead05f322c70f835314ac837
David Aznaurov <daznauro@redhat.com>
2026-05-04 14:06:27 -0400

    FCN-368: fixed section and custom styling

--- src/components/Wizards/ROSAHCPWizard/components/Section.tsx
+++ src/components/Wizards/ROSAHCPWizard/components/Section.tsx
@@ -1,6 +1,5 @@
-import { Content, Divider, Split, SplitItem, Stack } from '@patternfly/react-core';
+import { Content, Split, SplitItem, Stack } from '@patternfly/react-core';
 import { LabelHelp } from './LabelHelp';
-import { AngleDownIcon, AngleLeftIcon } from '@patternfly/react-icons';
 import React, { ReactNode } from 'react';
 
 type SectionProps = {
@@ -12,24 +11,15 @@ type SectionProps = {
   defaultExpanded?: boolean;
   labelHelpTitle?: string;
   labelHelp?: string;
-  collapsable?: boolean;
   autohide?: boolean;
 };
 
 export const Section: React.FunctionComponent<SectionProps> = (props) => {
   const id = props.id ?? props.label?.toLowerCase().split(' ').join('-') ?? '';
-  const [expanded, setExpanded] = React.useState(
-    props.defaultExpanded === undefined ? true : props.defaultExpanded
-  );
 
   return (
     <section id={id} className="pf-v6-c-form__group" role="group">
-      <Split
-        hasGutter
-        onClick={() => {
-          if (props.collapsable) setExpanded(!expanded);
-        }}
-      >
+      <Split hasGutter>
         <SplitItem isFilled>
           <Stack>
             <Split hasGutter>
@@ -44,30 +34,15 @@ export const Section: React.FunctionComponent<SectionProps> = (props) => {
                 )}
               </div>
             </Split>
-            {expanded && props.description && (
-              <Content component="small" style={{ paddingTop: 8 }}>
+            {props.description && (
+              <Content component="small" className="pf-v6-u-pt-sm">
                 {props.description}
               </Content>
             )}
           </Stack>
         </SplitItem>
-        {props.collapsable &&
-          (expanded ? (
-            <SplitItem>
-              <div style={{ marginBottom: -5 }}>
-                <AngleDownIcon />
-              </div>
-            </SplitItem>
-          ) : (
-            <SplitItem>
-              <div style={{ marginBottom: -5 }}>
-                <AngleLeftIcon />
-              </div>
-            </SplitItem>
-          ))}
       </Split>
-      {expanded ? props.children : <div style={{ display: 'none' }}>{props.children}</div>}
-      {!expanded && <Divider />}
+      {props.children}
     </section>
   );
 };
